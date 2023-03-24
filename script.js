dc7e94bdd6f1f22c7fd078bd7598b915614349bb
neomink <jungmin.kim.0711@gmail.com>
2023-03-23 22:18:48 -0400

    structure

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("hello")
+
+const robots = {
+    ironMan007: {
+        "english_title": "Iron Man 007",
+        "korean_title": "철인 007",
+        "year": "1974",
+        "director": "Ha-rim Han",
+        "production": "Dong-Ah Advertisement",
+        "duration": "64 minutes",
+        "images": "",
+        "youtube": "49F8xMCAjnU"
+    },
+    robotTaekwonV: {
+        "english_title": "Robot Taekwon V",
+        "korean_title": "로보트 태권 브이",
+        "year": "1976",
+        "director": "Chung-gi Kim",
+        "production": "U Production",
+        "duration": "85 minutes",
+        "images": "",
+        "youtube": "i4DF-L4kRCE"
+    }
+}
+
+// when I click Iron Man 007 button, I want the information to show in info box and videos and images to show in resources
