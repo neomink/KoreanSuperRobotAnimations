33631c47f3b3833c57f98e13217b9a790777c0d7
neomink <jungmin.kim.0711@gmail.com>
2023-03-24 11:25:35 -0400

    tlqkf!

--- script.js
+++ script.js
@@ -17,13 +17,21 @@ const robots = {
 
 //Iron Man 007
 function renderIronMan007(robot) {
-    const infoList = document.getElementById("container-info");
-    const ironMan007Info = document.createElement('li');
+    //title
+    const title = document.getElementById("title");
+    const ironMan007Title = document.createElement('div');
+    const titleHTML = `
+    <p class="info-title">${robots.ironMan007.english_title}</p>
+    `
+    ironMan007Title.innerHTML = titleHTML
+    title.appendChild(ironMan007Title)
 
+    //info
+    const infoList = document.getElementById("info");
+    const ironMan007Info = document.createElement('div');
     const infoHTML = `
     <ul>
-        <li class="info-title">${robots.ironMan007.english_title}</li>
-        <li class="info-list">${robots.ironMan007.korean_title}</li>
+        <li class="info-list">year:${robots.ironMan007.year}</li>
         <li class="info-list">director: ${robots.ironMan007.director}</li>
         <li class="info-list">production: ${robots.ironMan007.production}</li>
         <li class="info-list">duration: ${robots.ironMan007.duration}</li>
@@ -31,10 +39,17 @@ function renderIronMan007(robot) {
     </ul>
     <img src="iron-man-007.png" class="info-image">
     `
-
     ironMan007Info.innerHTML = infoHTML
-
     info.appendChild(ironMan007Info)
+
+    //video
+    const iframe = document.getElementById("videos")
+    const ironMan007Video  = document.createElement("div")
+    const videoHTML = `
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/${robots.ironMan007.youtube}?autoplay=1&mute=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    `
+    ironMan007Video.innerHTML = videoHTML 
+    videos.appendChild(ironMan007Video)
 }
 
 const ironMan007Button = document.getElementById("iron-man-007");
